2c1b9035ab2a6c72cddc59007ca5eff1a5b4d243
venkataegt <venkata.egt@gmail.com>
2020-08-19 03:04:17 -0400

    first commit

---
+++ src/main/java/com/gsa/aws/AwsElasticBeanstalkExample2Application.java
@@ -0,0 +1,13 @@
+package com.gsa.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkExample2Application.class, args);
+	}
+
+}
